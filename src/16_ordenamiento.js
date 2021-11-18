356218c6212cabaef07b55f12d466f1486223646
David Rodriguez <dalejorod@gmail.com>
2021-11-18 15:18:44 -0400

    add exercise 16 vol 1

--- src/16_ordenamiento.js
+++ src/16_ordenamiento.js
@@ -1,1 +1,10 @@
 import { input } from './utils/input.js';
+
+const firstNumber = parseInt(await input('Ingrese numero: '), 10);
+const secondNumber = parseInt(await input('Ingrese numero: '), 10);
+
+if (firstNumber > secondNumber){
+    console.log(firstNumber + ' ' + secondNumber);
+} else {
+    console.log(secondNumber + ' ' + firstNumber);
+}
