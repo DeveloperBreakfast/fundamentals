7432675cd7b72344dfe148ce011268052216ae39
David Rodriguez <dalejorod@gmail.com>
2021-11-19 00:04:03 -0400

    fix exercise 15

--- src/15_longest_word.js
+++ src/15_longest_word.js
@@ -1,18 +1,17 @@
 import { input } from './utils/input.js';
+import process from 'process'
 
-let primeraPalabra = await input('Palabra 1: ');
-let segundaPalabra = await input('Palabra 2: ');
+const primeraPalabra = await input('Palabra 1: ');
+const segundaPalabra = await input('Palabra 2: ');
 
-let palabra1Length = primeraPalabra.length;
-let palabra2Length = segundaPalabra.length;
+const palabra1Mayor = primeraPalabra.length - segundaPalabra.length;
+const palabra2Mayor = segundaPalabra.length - primeraPalabra.length;
+if (primeraPalabra.length === segundaPalabra.length){
+    console.log('Las dos palabras tienen el mismo largo');
+    process.exit();
+}
 
-const palabra1Mayor = palabra1Length - palabra2Length;
-const palabra2Mayor = palabra2Length - palabra1Length;
-
-if (palabra1Length > palabra2Length){
-    if(palabra1Mayor === palabra2Mayor){
-        console.log('Las dos palabras tienen el mismo largo');
-    } else 
+if (primeraPalabra.length > segundaPalabra.length){ 
     console.log(`La palabra ${primeraPalabra} tiene ${palabra1Mayor} letras mas que ${segundaPalabra}`);
 } else {
     console.log(`La palabra ${segundaPalabra} tiene ${palabra2Mayor} letras mas que ${primeraPalabra}`);
--- src/17_ordenamientos_vol2.js
+++ src/17_ordenamientos_vol2.js
@@ -1,10 +1,7 @@
+import { parse } from 'path/posix';
 import { input } from './utils/input.js';
 
 const firstNumber = parseInt(await input('Ingrese numero: '), 10);
 const secondNumber = parseInt(await input('Ingrese numero: '), 10);
+const thirdNumber = parseInt(await input('Ingrese numero: '), 10);
 
-if (firstNumber < secondNumber){
-    console.log(firstNumber + ' ' + secondNumber);
-} else {
-    console.log(secondNumber + ' ' + firstNumber);
-}
