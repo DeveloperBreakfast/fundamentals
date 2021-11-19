c453325c445f34e9b6fbaebc98a353b47aa60136
David Rodriguez <dalejorod@gmail.com>
2021-11-19 13:50:55 -0400

    add exercise 17

--- src/17_ordenamientos_vol2.js
+++ src/17_ordenamientos_vol2.js
@@ -1,7 +1,12 @@
-import { parse } from 'path/posix';
 import { input } from './utils/input.js';
 
 const firstNumber = parseInt(await input('Ingrese numero: '), 10);
 const secondNumber = parseInt(await input('Ingrese numero: '), 10);
 const thirdNumber = parseInt(await input('Ingrese numero: '), 10);
 
+const numerosOrdenados = [firstNumber, secondNumber, thirdNumber];
+numerosOrdenados.sort(function(a, b) {
+    return a - b;
+});
+ 
+console.log(numerosOrdenados);
