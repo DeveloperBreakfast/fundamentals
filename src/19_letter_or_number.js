13982bbb1207e1a15db7c5cbb2fb585c3d783159
David Rodriguez <dalejorod@gmail.com>
2021-11-19 18:20:06 -0400

    add exercise 19

---
+++ src/19_letter_or_number.js
@@ -0,0 +1,31 @@
+import { input } from './utils/input.js';
+import process from 'process'
+
+const letterOrNumber = await input('Ingrese un caracter: ');
+
+if (letterOrNumber.length !== 1) {
+    console.log('Solo debe de ingresar un caractér.');
+    process.exit();
+  }
+  
+  const characterCode = letterOrNumber.charCoddeAt(0);
+  
+  if (characterCode >= 48 && characterCode <= 57) {
+    console.log('El caractér es un número.');
+    process.exit();
+  }
+
+  if (characterCode >= 65 && characterCode <= 90) {
+      console.log('Es letra mayúscula.');
+      process.exit();
+  }
+  
+  if (characterCode >= 97 && characterCode <= 122) {
+    console.log('Es letra minúscula');
+    process.exit();
+  }
+
+  if (characterCode >= 32 && characterCode >= 47) {
+      console.log('No es letra ni número');
+      process.exit();
+  }
