819619d14543050fae8524f34b0dd3ad7c3ef03b
David Rodriguez <dalejorod@gmail.com>
2021-12-03 17:06:34 -0400

    Add exercise 27

---
+++ src/27_sum_between_numbers.js
@@ -0,0 +1,12 @@
+import { input } from './utils/input.js';
+
+const numOne = parseInt(await input('Ingrese num: '), 10);
+const numTwo = parseInt(await input('Ingrese num: '), 10);
+
+let total = 0
+
+for (let i = 0; i < numTwo; i++) {
+ total += i;   
+}
+
+console.log(`La suma es ${total}`);
